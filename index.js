843f7020ca9b40d7efa1c85fa382a934dba96817
joagonca <joaogoncalves@parceiro.sonae.pt>
2017-02-14 11:16:20 +0000

    #3 fixing nitpicks

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8080,
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 8080,
 
-    middleware = require('./middleware'),
-    product = require('./lib/product');
+      middleware = require('./middleware'),
+      product = require('./lib/product');
 
 app.use(middleware());
 app.get('/product/:pid', product());
